308c31d378c4dec344ec893ce838430a389b78c1
sevauni <sevauni@gmail.com>
2015-08-21 16:29:24 +0300

    b

--- samples/pcd8544_rpi.c
+++ samples/pcd8544_rpi.c
@@ -31,7 +31,7 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 Lesser General Public License for more details.
 ================================================================================
- */
+ */ 
 #include <wiringPi.h>
 #include <stdint.h>
 #include <stdio.h>
